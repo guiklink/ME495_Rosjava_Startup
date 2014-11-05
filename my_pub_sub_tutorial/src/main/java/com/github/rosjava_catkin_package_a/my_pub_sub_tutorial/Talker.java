e919381dd6dc1ebb646571aea1ec6836273f49fc
Guilherme Klink <guilherme@yahoo.com>
2014-11-05 15:32:05 -0600

    TurtleBot code commented.

--- my_pub_sub_tutorial/src/main/java/com/github/rosjava_catkin_package_a/my_pub_sub_tutorial/Talker.java
+++ my_pub_sub_tutorial/src/main/java/com/github/rosjava_catkin_package_a/my_pub_sub_tutorial/Talker.java
@@ -18,16 +18,16 @@ package com.github.rosjava_catkin_package_a.my_pub_sub_tutorial;
 
 import org.ros.concurrent.CancellableLoop;
 import org.ros.namespace.GraphName;
-import org.ros.node.AbstractNodeMain;
+import org.ros.node.AbstractNodeMain;  // This library give us the AbstractNodeMain interface (see ahead)
 import org.ros.node.ConnectedNode;
-import org.ros.node.NodeMain;
-import org.ros.node.topic.Publisher;
-import geometry_msgs.Twist;
+import org.ros.node.NodeMain;          
+import org.ros.node.topic.Publisher;  // Import the publisher
+import geometry_msgs.Twist;           // Import geometry_msgs.Twist ... remember to incluse this message into your dependencie files (see mini-project_rosjava.md)
 
 /**
  * A simple {@link Publisher} {@link NodeMain}.
  */
-public class Talker extends AbstractNodeMain {
+public class Talker extends AbstractNodeMain { // Java nodes NEEDS to implement AbstractNodeMain
 
   @Override
   public GraphName getDefaultNodeName() {
@@ -37,7 +37,7 @@ public class Talker extends AbstractNodeMain {
   @Override
   public void onStart(final ConnectedNode connectedNode) {
     final Publisher<geometry_msgs.Twist> publisher =
-        connectedNode.newPublisher("/turtle1/cmd_vel", geometry_msgs.Twist._TYPE);
+        connectedNode.newPublisher("/turtle1/cmd_vel", geometry_msgs.Twist._TYPE); // That's how you create a publisher in Java!
     // This CancellableLoop will be canceled automatically when the node shuts
     // down.
     connectedNode.executeCancellableLoop(new CancellableLoop() {
@@ -50,19 +50,19 @@ public class Talker extends AbstractNodeMain {
 
       @Override
       protected void loop() throws InterruptedException {
-        geometry_msgs.Twist twist = publisher.newMessage();
+        geometry_msgs.Twist twist = publisher.newMessage(); // Init a msg variable that of the publisher type
         sequenceNumber++;
 
-        if (sequenceNumber % 5 == 0) {
-          twist.getAngular().setZ(Math.PI/2);  
+        if (sequenceNumber % 3 == 0) {          // Every 3 executions of the loop (aprox. 3*1000ms = 3 sec)
+          twist.getAngular().setZ(Math.PI/2);   // Steer the turtle left
         }
         else{
-          twist.getLinear().setX(2);
+          twist.getLinear().setX(2);            // In the meantime keeps going foward 
         }
         
-        publisher.publish(twist);
+        publisher.publish(twist);       // Publish the message (if running use rostopic list to see the message)
 
-        Thread.sleep(1000);
+        Thread.sleep(1000);             // Sleep for 1000 ms = 1 sec
       }
     });
   }
